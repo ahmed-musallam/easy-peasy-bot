21fb65d8e9e88f57b7245873b97d84fc23e939bd
Ahmed Musallam <atyaf66@gmail.com>
2018-07-27 15:53:27 -0500

    add plus/minus for users and things! and a ranking board

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 /**
  * A Bot for Slack!
  */
+
 /**
  * Define a function for initiating a conversation on installation
  * With custom integrations, we don't have a way to find out who installed us, so we can't message them :(
  */
-
 function onInstallation (bot, installer) {
   if (installer) {
     bot.startPrivateConversation({ user: installer }, function (err, convo) {
@@ -19,52 +19,52 @@ function onInstallation (bot, installer) {
 }
 
 /**
- * Configure the persistence options
+ * Get initial config for controller
  */
-
-var config = {}
-if (process.env.MONGOLAB_URI) {
-  var BotkitStorage = require('botkit-storage-mongo')
-  config = {
-    storage: BotkitStorage({ mongoUri: process.env.MONGOLAB_URI })
+function getConfig () {
+  let config
+  if (process.env.MONGOLAB_URI) {
+    var BotkitStorage = require('botkit-storage-mongo')
+    config = {
+      storage: BotkitStorage({ mongoUri: process.env.MONGOLAB_URI })
+    }
+  } else {
+    config = {
+      json_file_store: ((process.env.TOKEN) ? './db_slack_bot_ci/' : './db_slack_bot_a/') // use a different name if an app or CI
+    }
   }
-} else {
-  config = {
-    json_file_store: ((process.env.TOKEN) ? './db_slack_bot_ci/' : './db_slack_bot_a/') // use a different name if an app or CI
-  }
+  return config
 }
 
 /**
- * Are being run as an app or a custom integration? The initialization will differ, depending
+ * Are we being run as an app or a custom integration? The initialization will differ, depending
  */
-var controller
-if (process.env.TOKEN || process.env.SLACK_TOKEN) {
-  // Treat this as a custom integration
-  var customIntegration = require('./lib/custom_integrations')
-  var token = (process.env.TOKEN) ? process.env.TOKEN : process.env.SLACK_TOKEN
-  controller = customIntegration.configure(token, config, onInstallation)
-} else if (process.env.CLIENT_ID && process.env.CLIENT_SECRET && process.env.PORT) {
-  // Treat this as an app
-  var app = require('./lib/apps')
-  controller = app.configure(process.env.PORT, process.env.CLIENT_ID, process.env.CLIENT_SECRET, config, onInstallation)
-} else {
-  console.log('Error: If this is a custom integration, please specify TOKEN in the environment. If this is an app, please specify CLIENTID, CLIENTSECRET, and PORT in the environment')
-  process.exit(1)
+function configureController (config) {
+  var controller
+  if (process.env.TOKEN || process.env.SLACK_TOKEN) {
+    // Treat this as a custom integration
+    var customIntegration = require('./lib/custom_integrations')
+    var token = (process.env.TOKEN) ? process.env.TOKEN : process.env.SLACK_TOKEN
+    controller = customIntegration.configure(token, config, onInstallation)
+  } else if (process.env.CLIENT_ID && process.env.CLIENT_SECRET && process.env.PORT) {
+    // Treat this as an app
+    var app = require('./lib/apps')
+    controller = app.configure(process.env.PORT, process.env.CLIENT_ID, process.env.CLIENT_SECRET, config, onInstallation)
+  } else {
+    console.log('Error: If this is a custom integration, please specify TOKEN in the environment. If this is an app, please specify CLIENTID, CLIENTSECRET, and PORT in the environment')
+    process.exit(1)
+  }
+  return controller
 }
 
-/**
- * A demonstration for how to handle websocket events. In this case, just log when we have and have not
- * been disconnected from the websocket. In the future, it would be super awesome to be able to specify
- * a reconnect policy, and do reconnections automatically. In the meantime, we aren't going to attempt reconnects,
- * WHICH IS A B0RKED WAY TO HANDLE BEING DISCONNECTED. So we need to fix this.
- *
- * TODO: fixed b0rked reconnect behavior
- */
+/****************************
+ *          MAIN
+ ***************************/
+const controller = configureController(getConfig())
 // Handle events related to the websocket connection to Slack
 controller.on('rtm_open', function (bot) {
   console.log('** The RTM api just connected!')
 })
-
 controller.on('rtm_close', function (bot) {
   console.log('** The RTM api just closed')
   // you may want to attempt to re-open
@@ -77,18 +77,5 @@ controller.on('bot_channel_join', function (bot, message) {
 controller.hears('hello', 'direct_message', function (bot, message) {
   bot.reply(message, 'Hello!')
 })
-
-var REGEX = /@([^ ]+)[ ]*\+\+/g
-controller.hears(REGEX.source, 'direct_message', function (bot, message) {
-  console.log(JSON.stringify(message))
-  controller.storage.users.get(message.user, function (err, data) {
-    if (err) console.log(`could not find user ${message.user}`)
-    data = Object.assign({ score: 0 }, data)
-    const userName = message.match[1]
-    const newScore = data.score + 1
-    controller.storage.users.save({ id: userName, score: newScore }, function (err) {
-      if (err) console.log(err)
-      bot.reply(message, `[@${userName}] now at ${newScore} points`)
-    })
-  })
-})
+const Shuriken = require('./lib/shuriken')
+const shuriken = new Shuriken(controller)
